3a20476b12f69fc8f2d36ea42f6baecd521867ff
brian <brianchng07@outlook.com>
2025-06-02 21:50:51 +0800

    gtre

--- t.py
+++ t.py
@@ -12,25 +12,28 @@ def generate(selected):
               '507-contemporary-folk', '132-dance', '6-electronic', '263-edm', '9-experimental', '5-folk',
               '49-hardcore-punk', '3-hip-hop', '4-indie-pop', '1-indie-rock', '35-jazz', '40-metal', '15-pop',
               '103-pop-rock', '28-punk', '22-r-and-b', '7-rock', '213-trap-rap']
-
-    a = [genres[int(i)] for i in selected]
-    genre = a[random.randint(0,len(a)-1)]
-    url = f'{url}/genre/{genre}/all' if genre != genres[0] else f'{url}/ratings/{genre}/all/1'
-    def get_soup(url):
-        headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Referer': 'https://www.albumoftheyear.org/',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Connection': 'keep-alive',
-        }
-        session = requests.Session()
-        session.headers.update(headers)
-        response = scraper.get(url)
-        return BeautifulSoup(response.text, "html.parser")
-    soup = get_soup(url)
-    pages = soup.find('div', class_ = "pageSelectRow").find_all()[-1].text
-
+    while True:
+        try:
+            a = [genres[int(i)] for i in selected]
+            genre = a[random.randint(0,len(a)-1)]
+            url = f'{url}/genre/{genre}/all' if genre != genres[0] else f'{url}/ratings/{genre}/all/1'
+            def get_soup(url):
+                headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Referer': 'https://www.albumoftheyear.org/',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Connection': 'keep-alive',
+                }
+                session = requests.Session()
+                session.headers.update(headers)
+                response = scraper.get(url)
+                return BeautifulSoup(response.text, "html.parser")
+            soup = get_soup(url)
+            pages = soup.find('div', class_ = "pageSelectRow").find_all()[-1].text
+            break
+        except:
+            time.sleep(2)
     time.sleep(2)
 
     while True:
@@ -44,8 +47,11 @@ def generate(selected):
             cover = pick.find('div', class_= 'albumListCover').find('a').find('img').get('data-src')
             date = pick.find('div', class_= 'albumListDate').text
             genres = pick.find('div', class_= 'albumListGenre').text
-            print( name, cover, date, genres)
-            return {"name": name, "cover": cover, "date": date, "genres": genres}
+            links = pick.find('div', class_ = 'albumListLinks').find_all('a')
+            am = links[1].get('href')
+            spotify = links[2].get('href')
+            print( name, cover, date, genres, am, spotify)
+            return {"name": name, "cover": cover, "date": date, "genres": genres, "am": am, "spotify": spotify}
             break
         except:
             time.sleep(2)
